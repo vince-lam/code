b854bda6d01ff9587cd288d275c984547036c90a
Vincent Lam <57117625+vincentlam13@users.noreply.github.com>
2020-07-05 18:41:35 +0100

    Add files via upload

---
+++ SQL/1_BasicSQL/30_Arithmetic Operators.sql
@@ -0,0 +1,10 @@
+#Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the results to the first 10 orders, and include the id and account_id fields.
+SELECT id, account_id, standard_amt_usd / standard_qty AS unit_price
+FROM orders
+LIMIT 10;
+
+
+# Write a query that finds the percentage of revenue that comes from poster paper for each order. You will need to use only the columns that end with _usd. (Try to do this without using the total column.) Display the id and account_id fields also.
+SELECT id, account_id, poster_amt_usd / total_amt_usd * 100 AS post_per
+FROM orders
+LIMIT 10;
---
+++ SQL/1_BasicSQL/34_LIKE.sql
@@ -0,0 +1,14 @@
+# All the companies whose names start with 'C'.
+SELECT *
+FROM accounts
+WHERE name LIKE 'C%'
+
+# All companies whose names contain the string 'one' somewhere in the name.
+SELECT *
+FROM accounts
+WHERE name LIKE '%one%'
+
+# All companies whose names end with 's'.
+SELECT *
+FROM accounts
+WHERE name LIKE '%s'
