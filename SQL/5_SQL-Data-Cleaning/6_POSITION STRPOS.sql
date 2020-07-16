0e4d8581320679226999ca55e6c45bf2e8057f39
Vincent Lam <57117625+vincentlam13@users.noreply.github.com>
2020-07-16 15:24:00 +0100

    Add files via upload

---
+++ SQL/5_SQL-Data-Cleaning/6_POSITION STRPOS.sql
@@ -0,0 +1,18 @@
+/* 1) Use the accounts table to create first and last name columns
+that hold the first and last names for the primary_poc. */
+
+SELECT primary_poc, LEFT(UPPER(primary_poc), STRPOS(primary_poc, ' ')), RIGHT(UPPER(primary_poc), STRPOS(primary_poc, ' '))
+FROM accounts
+
+
+SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ')-1) first_name,
+RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name
+FROM accounts;
+
+
+/* Now see if you can do the same thing for every rep name in the
+sales_reps table. Again provide first and last name columns. */
+
+SELECT LEFT(name, STRPOS(name, ' ') -1 ) first_name,
+       RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) last_name
+FROM sales_reps;
